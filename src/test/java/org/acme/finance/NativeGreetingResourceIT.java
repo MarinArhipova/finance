6e04170eaee79104a38ab7bd182e3f2371f87871
Octanty Mulianingtyas <octantyas@gmail.com>
2020-12-10 17:18:07 +0100

    install quarkus

---
+++ src/test/java/org/acme/finance/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.finance;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
