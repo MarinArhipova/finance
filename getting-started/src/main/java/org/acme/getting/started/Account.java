6526de2995bc0fc1f0d04d5431ed1705b8cd5a39
Octanty Mulianingtyas <octantyas@gmail.com>
2021-01-07 16:20:11 +0700

    update entity in database to one to many

--- getting-started/src/main/java/org/acme/getting/started/Account.java
+++ getting-started/src/main/java/org/acme/getting/started/Account.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
 
-@Entity(name = "accounts")
+@Entity
 //@Cacheable
 public class Account extends PanacheEntity {
 
